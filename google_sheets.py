6469f94723d613f297a0f7b4a6522e356480d7fc
Dennis <mr.denis.dav@mail.ru>
2024-08-27 18:30:38 +0530

    file G sheets

--- google_sheets.py
+++ google_sheets.py
@@ -1,42 +1,34 @@
-from __future__ import print_function
-import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+import os
 
-# If modifying these scopes, delete the file token.json.
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1GzJuuHlr6858tv6HgjBkAhSe48JHhQ8ZeTAZApxAwdE'
-SAMPLE_RANGE_NAME = 'New Coins!A2'
-        
-def write_to_google_sheets(data):
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+SERVICE_ACCOUNT_FILE = 'another.json'
+SERVICE_ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), SERVICE_ACCOUNT_FILE)
 
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'key.json', SCOPES)
-            creds = flow.run_local_server(port=3000)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    try:
-        service = build('sheets', 'v4', credentials=creds)
-        sheet = service.spreadsheets()
-        result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME,valueInputOption="USER_ENTERED",body={"values":data}).execute()
-        print(result)
-    except HttpError as err:
-        print(err)
+SAMPLE_SPREADSHEET_ID = '1UHL0RqUcDh7DqDz76IGPcLPgqSlkFzMenVZ3Y8LoRJk'
+SAMPLE_RANGE_NAME = 'New Coins'
+
+def write_to_google_sheets(data, cell='A1'):
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    range_name = f"{SAMPLE_RANGE_NAME}!A:A"  # Assuming the column A is used for row numbers
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=range_name).execute()
+    values = result.get('values', [])
+    last_row = len(values) + 1
+    cell='A'+str(last_row)
+    range_name = f"{SAMPLE_RANGE_NAME}!{cell}"
+    print(range_name)
+    result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                   range=range_name,
+                                   valueInputOption="USER_ENTERED",
+                                   body={"values":data}).execute()
+    print(result)
+
+to_write = [['testiiiiim','2222']]
+write_to_google_sheets(to_write, cell='A6')
--- test_pumpPortal.py
+++ test_pumpPortal.py
@@ -2,31 +2,59 @@ import asyncio
 import websockets
 import json
 
+from google_sheets import write_to_google_sheets
+
+wallet_addresses = [
+    "DS2KkjMazkU5rN6E2KMMzTS5ct8aQVP2ruBf57K4c1FF",
+    "4tK7RdcseCvPxAQZiv7iAzyqawedEe16dZb1FZ39zarj",
+    "8TKbC2hE5RwnXUNe2b2a6e5UmRdxPTGS5LX6B2RWAv83",
+    "G5vWA8juZKcEcNBq6noaYC25kNEy1f5NWY9aaBHF8Cno",
+    "AYgZ8C6P11c8iTCj2YyANT9Xok6XUm7iZ7BCeR7fW3XL",
+    "CWvdyvKHEu8Z6QqGraJT3sLPyp9bJfFhoXcxUYRKC8ou",
+    "2RssnB7hcrnBEx55hXMKT1E7gN27g9ecQFbbCc5Zjajq",
+    "DY5Ej41PGDWsDoxMJPbrewTjzyVPLmm5kNL5Bsw3mMVX",
+    "4ADFwrmsrdSVUx8cp7mVtcynvM3hFcdkBZk4vi83KnrA",
+    "4K1QY4aSGveSZ5UUpeG6LP6JsqkSZPvBMo3JD2YTmCCE",
+    "nAxHm1ZmiSbu9kPM4orssuRRUqCJGWE4rzEtYpa5yTp",
+    "GQWLRHtR18vy8myoHkgc9SMcSzwUdBjJ816vehSBwcis",
+    "39PcuE9MBnW4FnqcDJYiy4ppRApdDq4XN4bfksG2nEuP",
+    "BWQMaBZXLTH3RVvRZjwKhQFTahmEtZiSiyWkA94F4oHu",
+    "4PkUrVUp99BwePxDvvgoGVYZM213GyE9VR8Z5235sbVe",
+    "5ePaQ8YreTMQbNoBwiFe6mbvBLBYoGFek3U3pC8TVVsw",
+    "JDZyJqZh6vwqnCeGBXymXPFkMTqDNAGJnm7rz1t6BTnQ",
+    "6bKUguaAgYYbxwvzg7ge4vNTHtrVTmKBSfJRWVvNwAPr",
+    "Ec4BAURantJU6P2yn7HQLxZq9bUo4ZFmc5DAJhVMM1oc",
+    "sEVggxf69nwprkgCPpLMq5i4eAYxYutxCLBiLn5EWRq",
+    "Haq5TB66joFhjKGGSBi6Sdk3Rh9xX4We6wCePGV5uArP",
+    "Bq2RWav5LtfnAs8refzrYjfroDGK9vjYULmTqJX7yXfJ",
+    "5QemTzE4eBsfpN9XjTHmQLVwKjDkdUjcAJKEL4JW9Vgv",
+    "gEwDSjhP1xJFUwiaZDjTNvaTCtKhBjybR2nxPrHzkGc",
+    "H1iBiJj595JhzeQaeudJrfBvMSRKiLNC2NQytrbfW3hU"
+]
 CA = '5FMjMuiAdgwF3REQogMqrdLRBF9pKs5wfJ9hW71Fpump'
-async def subscribe():
+
+async def subscribe(payload):
     uri = "wss://pumpportal.fun/api/data"
     async with websockets.connect(uri) as websocket:
-        
-        # Subscribing to token creation events
-        payload = {
-            "method": "subscribeNewToken",
-        }
-
-        # Subscribing to trades made by accounts
-        # payload = {
-        #     "method": "subscribeAccountTrade",
-        #     "keys": ["AArPXm8JatJiuyEffuC1un2Sc835SULa4uQqDcaGpAjV"]  # array of accounts to watch
-        # }
-
-        # Subscribing to trades on tokens
-        payload = {
-            "method": "subscribeTokenTrade",
-            "keys": [CA]  # array of token CAs to watch
-        }
         await websocket.send(json.dumps(payload))
         
         async for message in websocket:
-            print(json.loads(message))
+            dict = json.loads(message)
+            print(dict)
+            if 'name' in dict:
+                print(f'message1 {dict["name"]} {dict["marketCapSol"]} ')
+                write_to_google_sheets([[dict["marketCapSol"],dict["name"]]])
 
-    # Run the subscribe function
-asyncio.get_event_loop().run_until_complete(subscribe())
+payload2 = {
+            "method": "subscribeNewToken",
+        }
+payload = {
+            "method": "subscribeTokenTrade",
+            "keys": [CA]  # array of token CAs to watch
+        }
+payload3 = {
+        "method": "subscribeAccountTrade",
+        "keys": wallet_addresses  # array of accounts to watch
+    }
+
+asyncio.get_event_loop().run_until_complete(subscribe(payload3))
